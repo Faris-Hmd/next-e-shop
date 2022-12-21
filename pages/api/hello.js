98587d933f2d06f626094ffb09d84387f163f3a0
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2022-12-21 12:00:35 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+const getOffers = async () => {
+    // console.log("get offers")
+    const querySnapshot = await getDocs(
+      query(collection(db, "products"), where("isOffer", "==", true))
+    );
+const arr = [];
+    querySnapshot.forEach((doc) =>
+      arr.push({ ...doc.data(), id: doc.id })
+    );
+  res.status(200).json({ arr})
 }
